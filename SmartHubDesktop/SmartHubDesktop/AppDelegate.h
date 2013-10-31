3144ac6fe2148e48cb4756e823d64d372f064f21
Robbie Plankenhorn <rplankenhorn@gmail.com>
2013-10-31 11:15:33 -0500

    Added SmartHubDesktop project

---
+++ SmartHubDesktop/SmartHubDesktop/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SmartHubDesktop
+//
+//  Created by Robbie Plankenhorn on 10/31/13.
+//  Copyright (c) 2013 Solstice Mobile. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ SmartHubDesktop/SmartHubDesktop/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  SmartHubDesktop
+//
+//  Created by Robbie Plankenhorn on 10/31/13.
+//  Copyright (c) 2013 Solstice Mobile. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
